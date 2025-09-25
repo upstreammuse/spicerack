8f621c429779408528f61ffb785a05d25b4eee73
Michael <upstreammuse@users.noreply.github.com>
2025-09-25 11:22:58 -0400

    AndGate uses original structure names

--- andgate.c
+++ andgate.c
@@ -6,21 +6,17 @@
 #include <assert.h>
 #include <stdlib.h>
 
-typedef struct AndGate ANDGATE;
-typedef struct Signal SIGNAL;
-typedef enum SignalValue SIGVAL;
-
 static void andGateHandler(void*);
 
 struct AndGate {
-   SIGNAL* A;
-   SIGNAL* B;
-   SIGNAL* O;
    int OID;
+   struct Signal* A;
+   struct Signal* B;
+   struct Signal* O;
 };
 
-ANDGATE* andGateNew(void) {
-   ANDGATE* gate = malloc(sizeof (ANDGATE));
+struct AndGate* andGateNew(void) {
+   struct AndGate* gate = malloc(sizeof (struct AndGate));
    gate->A = signalNew(gate, andGateHandler);
    gate->B = signalNew(gate, andGateHandler);
    gate->O = NULL;
@@ -28,35 +24,33 @@ ANDGATE* andGateNew(void) {
    return gate;
 }
 
-void andGateFree(ANDGATE* gate) {
+void andGateFree(struct AndGate* gate) {
    if (gate == NULL) return;
    signalFree(gate->A);
    signalFree(gate->B);
    free(gate);
 }
 
-void andGateConnect(ANDGATE* gate, SIGNAL* output) {
+void andGateConnect(struct AndGate* gate, struct Signal* output) {
    assert(gate != NULL);
    assert(output != NULL);
    assert(gate->O == NULL);
    gate->O = output;
 }
 
-SIGNAL* andGateInputA(ANDGATE* gate) {
+struct Signal* andGateInputA(struct AndGate* gate) {
    assert(gate != NULL);
    return gate->A;
 }
 
-SIGNAL* andGateInputB(ANDGATE* gate) {
+struct Signal* andGateInputB(struct AndGate* gate) {
    assert(gate != NULL);
    return gate->B;
 }
 
 static void andGateHandler(void* block) {
-   ANDGATE* gate = block;
-   SIGVAL A;
-   SIGVAL B;
-   SIGVAL O = UNKNOWN;
+   struct AndGate* gate = block;
+   enum SignalValue A, B, O = UNKNOWN;
 
    assert(gate != NULL);
    if (gate->O == NULL) return;
