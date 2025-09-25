218bed9bfa9078719b9dbdebc87395e403ec1ecd
Michael <upstreammuse@users.noreply.github.com>
2025-09-25 11:10:04 -0400

    document Output a bit

--- output.c
+++ output.c
@@ -2,8 +2,15 @@
 
 #include <assert.h>
 
+/* remember the next output ID to provide */
 static unsigned int nextOutput = 1;
 
+/*
+ Give the next available output ID to a requestor.  Output IDs are used by
+ writers when writing to a signal to ensure that the same output continues to
+ write to the same signal, and to make sure that a signal is always written by
+ the same output.
+ */
 unsigned int allocateOutput(void) {
    assert(nextOutput != 0);
    return nextOutput++;
--- output.h
+++ output.h
@@ -1,3 +1,4 @@
 #pragma once
 
+/* get an output ID to use for writing a signal */
 unsigned int allocateOutput(void);
