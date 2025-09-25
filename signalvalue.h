4b2fbf2d4556a9456a3b3bfb9feeea7ae14f55d4
Michael <upstreammuse@users.noreply.github.com>
2025-09-25 11:04:55 -0400

    document SignalValue a bit

--- signalvalue.h
+++ signalvalue.h
@@ -1,5 +1,6 @@
 #pragma once
 
+/* The values that a signal can have */
 enum SignalValue {
    UNKNOWN,
    LOW,
@@ -7,4 +8,5 @@ enum SignalValue {
    HIGH_Z
 };
 
+/* Get a signal value as a human readable string */
 char* signalValueStr(enum SignalValue);
